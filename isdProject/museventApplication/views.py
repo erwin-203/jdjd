a55fbc1d689557a97c5726ec3ec8815e84c6d0c4
ismael <99xmr@protonmail.com>
2022-05-18 16:42:32 +0200

    req

--- isdProject/museventApplication/views.py
+++ isdProject/museventApplication/views.py
@@ -13,39 +13,53 @@ class CantonViewSet(viewsets.ModelViewSet):
     queryset = Canton.objects.all()
     serializer_class = CantonSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filter_fields = '__all__'
 
 
 class VisitorViewSet(viewsets.ModelViewSet):
     queryset = Visitor.objects.all()
     serializer_class = VisitorSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filter_fields = '__all__'
 
 
 class PlaceViewSet(viewsets.ModelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filter_fields = '__all__'
 
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filter_fields = '__all__'
 
 
 class RateEventViewSet(viewsets.ModelViewSet):
     queryset = RateEvent.objects.all()
     serializer_class = RateEventSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filter_fields = '__all__'
 
 
 class RatePlaceViewSet(viewsets.ModelViewSet):
     queryset = RatePlace.objects.all()
     serializer_class = RatePlaceSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filter_fields = '__all__'
 
 
 class ParticipationViewSet(viewsets.ModelViewSet):
     queryset = Participation.objects.all()
     serializer_class = ParticipationSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filter_fields = '__all__'
